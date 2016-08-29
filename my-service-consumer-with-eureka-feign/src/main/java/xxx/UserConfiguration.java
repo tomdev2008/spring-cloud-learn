b3a544dbb9b8fb6ece9027489da547c6c3b1e60f
dwb <dwb@dwb-571-00099>
2016-08-29 21:55:22 +0800

    配置retryer and options

--- my-service-consumer-with-eureka-feign/src/main/java/xxx/UserConfiguration.java
+++ my-service-consumer-with-eureka-feign/src/main/java/xxx/UserConfiguration.java
@@ -6,17 +6,30 @@ package xxx;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import feign.Feign;
 import feign.Logger;
+import feign.Request.Options;
+import feign.Retryer;
 
 /**
  * @author Zhu
  * @date 2016年7月28日 下午2:15:30
- * @description 
+ * @description
  */
 @Configuration
 public class UserConfiguration {
-    @Bean
-    Logger.Level feignLoggerLevel() {
-        return Logger.Level.FULL;
-    }
+	@Bean
+	Logger.Level feignLoggerLevel() {
+		return Logger.Level.FULL;
+	}
+
+	@Bean
+	public Retryer retryer() {
+		return new Retryer.Default(0, 0, 0);
+	}
+	
+	@Bean
+	public Options options(){
+		return new Options(10 * 1000, 1 * 1000);
+	}
 }
